331caaa898be11f9a38ff04b65f6aba5ae584b32
TayyabAfzal <tayyabafzal174@gmail.com>
2024-08-19 15:45:00 +0500

    upd path

--- components/Home/HomeProducts.jsx
+++ components/Home/HomeProducts.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import ProductsCard from '../Products/productsCard';
+import ProductsCard from '@/components/Products/ProductsCard';
 import { client } from '@/sanity/lib/client';
 
 export const revalidate = 60;
